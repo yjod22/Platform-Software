f26023459aea9553ea984b7da74185d9bfa8f0c3
Junseok Oh <yjod22@naver.com>
2024-12-20 22:31:23 +0100

    change baud rate from 115200 to 921600

--- EHAL/usart/inc/EhalUsart_OUT.h
+++ EHAL/usart/inc/EhalUsart_OUT.h
@@ -57,7 +57,7 @@ static inline void EhalUsart_OUT_InitUsart(void)
     USART_InitTypeDef USART_InitStructure;
 
     /* USARTx configuration ------------------------------------------------------*/
-    USART_InitStructure.USART_BaudRate   = 115200;
+    USART_InitStructure.USART_BaudRate   = 921600;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits   = USART_StopBits_1;
     USART_InitStructure.USART_Parity     = USART_Parity_No ;
